7feac021ee021b8499df7d9dc2dca42e20c9667b
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-05 22:58:01 -0700

    Add if else statements to turn question answers into true or false variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,12 +4,25 @@ puts "What is your full name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
-birth_year = gets.chomp
+birth_year = gets.chomp.to_i
 
 puts "We love garlic bread here. Should we order some for you?"
 garlic = gets.chomp
 
 puts "Would you like to enroll in the company's health coverage?"
 health_insurance = gets.chomp
+
+
+if age == (2016 - birth_year)
+  age = true
+else
+  age = false
+end
+
+if health_insurance == "yes" || health_insurance == "Yes" || health_insurance == "y" || health_insurance == "Y"
+	health_insurance = "y"
+else
+	health_insurance = "n"
+end
