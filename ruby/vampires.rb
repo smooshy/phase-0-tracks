7172cd5b1ead7afc3ff7330539951a8adf5defa5
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-05 20:55:39 -0700

    Add questionaire to vampires.rb to store answers

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+# Questionaire for new hires
+
+puts "What is your full name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "We love garlic bread here. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health coverage?"
+health_insurance = gets.chomp
