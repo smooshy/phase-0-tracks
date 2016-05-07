fe6fbf00d08855653ef35601529864a002cd9243
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-07 11:37:57 -0700

    Update conditional logic statements and order

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,37 +1,37 @@
 # Questionaire for new hires
+# Gather employee information
 
-puts "How many new hires will be processed today?"
-employees_processed = gets.chomp.to_i
-
-new_hires = 1
-while new_hires <= employees_processed
-
-  puts "What is your full name?"
+puts "What is your full name?"
   name = gets.chomp
+
+  if name == "Drake Cula" || name == "Tu Fang"
+  	name = false
+  else
+  	name = true
+  end
 
   puts "How old are you?"
   age = gets.chomp.to_i
   puts "What year were you born?"
   birth_year = gets.chomp.to_i
 
-  puts "We love garlic bread here. Should we order some for you?"
-  garlic = gets.chomp
-
-  puts "Would you like to enroll in the company's health coverage?"
-  health_insurance = gets.chomp
-
-
-  if age == (2016 - birth_year)
+   if age == (2016 - birth_year)
     age = true
   else
     age = false
   end
 
-  if garlic == "yes" || garlic == "Yes" || garlic == "y" || garlic == "Y"
+  puts "We love garlic bread here. Should we order some for you?"
+  garlic = gets.chomp
+
+   if garlic == "yes" || garlic == "Yes" || garlic == "y" || garlic == "Y"
   	garlic = true
   else
   	garlic = false
   end
+
+  puts "Would you like to enroll in the company's health coverage?"
+  health_insurance = gets.chomp
 
   if health_insurance == "yes" || health_insurance == "Yes" || health_insurance == "y" || health_insurance == "Y"
   	health_insurance = true
@@ -39,17 +39,18 @@ while new_hires <= employees_processed
   	health_insurance = false
   end
 
-  if age == true && (garlic == true || health_insurance == true)
+   if name == false
+      puts "Definitely a vampire"
+  elsif age && (garlic || health_insurance)
       puts "Probably not a vampire"
   elsif age == false && (garlic == false || health_insurance == false)
       puts "Probably a vampire"
   elsif age == false && garlic == false && health_insurance == false
       puts "Almost certainly a vampire"
-  elsif name == "Drake Cula" || name == "Tu Fang"
-      puts "Definitely a vampire"
   else
       puts "Results inconclusive"
   end
+
   new_hires += 1
 end
 
