5606a44b29c5f9f1a8cda46846c74ba1bcc9ab7c
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-06 19:42:23 -0700

    Add encryption psuedocode to secret agents file

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,7 @@
+# Encrypt Method
+# - Ask user for string
+# - Set response to user_input
+# - While user_input index is less than user_input length
+#   - return user_input index and advance to next letter
+# - End Encrypt
+# - Print encrypted user_input to screen
