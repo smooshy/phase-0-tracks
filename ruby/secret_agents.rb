ab8348215a2c400306a8235f5d27e37738a08f03
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-07 13:40:29 -0700

    Add encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -14,3 +14,20 @@
 #   - return user_input index and advance to the next letter in the alphabet.reverse
 # - End Decrypt
 # - Print decrypted user_input to screen
+
+
+puts "Please provide your password"
+  user_input = gets.chomp
+
+# Encrypt Method
+def encrypt(password)
+  index = 0
+  while index < password.length
+    password[index] = password[index].next!
+    index += 1
+  end
+  puts password
+end
+encrypt(user_input)
+
+# Decrypt Method
