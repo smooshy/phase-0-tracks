5681c77dc383da82a3a345a4ffa756691db6fd46
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-09 18:26:20 -0700

    Add string method line 7 and line 25 to get required result

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,8 +4,8 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+ => “InVeStIgAtIoN”
 
 # "zom".<???>
 # => “zoom”
@@ -22,8 +22,8 @@
 # " suspects".<???>
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+ => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
@@ -32,7 +32,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
