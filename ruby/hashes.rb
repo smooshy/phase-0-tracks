3976e19d9567ccfb3ede42571825c38764bd516c
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-11 22:12:41 -0700

    Add questions for name, age and number of children

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,10 +7,17 @@
 #   - allow designer to re-enter value for a specific question and print updated client information.
 #   - else end program
 
-
+# Initialize hash
 client_information = {}
 
+# Questions to ascertain client information
 puts "What is the client's full name?"
 client_information[:name] = gets.chomp
+
+puts "What is your age?"
+client_information[:age] = gets.chomp.to_i
+
+puts "How many children do you have?"
+client_information[:children] = gets.chomp.to_i
 
 p client_information
