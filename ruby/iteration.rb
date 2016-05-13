eaf82e6072249fcfcef431e1206e7a10a53e3c80
Andrew Peterson <ajpeterson79@gmail.com>
2016-05-12 18:58:56 -0700

    Add array and hash and iterate through array with .each

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,10 +1,28 @@
-def name
-  puts "What is your name?"
-  user_input = gets.chomp
-  puts "Our block is about to run."
-  yield (user_input)
-  puts "Our block is finished running."
+# Own method that can take a block
+#def name
+#  puts "What is your name?"
+#  user_input = gets.chomp
+#  puts "Our block is about to run."
+#  yield (user_input)
+#  puts "Our block is finished running."
+#end
+
+
+#name { |user_input| puts "Your name is #{user_input}."}
+
+
+# Release 1
+array = [9, 'hi', 'yes', 19]
+
+puts "Our original array:"
+p array
+
+array.each do | i |
+  puts i
 end
 
+puts "Our after .each method call:"
+p array
 
-name { |user_input| puts "Your name is #{user_input}."}
+
+#hash = {seattle => 'sounders', chicago => 'fire', portland => 'timbers', vancouver => #'whitecaps'}
